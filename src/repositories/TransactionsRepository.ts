de2a178284c1e14e2abdd21c2d952de5e4b6d9b6
Luan Ramos <luanramossfarias@gmail.com>
2020-11-04 22:54:38 -0300

    first commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionDTO{
+  title:string;
+  value:number;
+  type:'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,16 +19,58 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+  public getBalance(){
+   const transactions = this.transactions;
+   const reducer = (accumulator:any, currentValue: any) =>{
+    return accumulator + currentValue;}
 
-  public create(): Transaction {
-    // TODO
-  }
+    function arrayConverter(type:"outcome"|"income",transaction:Transaction[]){
+      const arr = transaction.map(transaction=>{
+        if(transaction.type==type){
+          return transaction.value;
+          }else{
+          return 0
+            }})
+        return arr;}
+
+      const arrayIncome = arrayConverter("income",transactions)
+      const arrayOutcome = arrayConverter("outcome",transactions)
+      const income = arrayIncome.reduce(reducer);
+      const outcome = arrayOutcome.reduce(reducer);
+      const total = income-outcome;
+
+
+  return ({income,outcome,total});
+
 }
+
+
+  public create({title,value,type}:CreateTransactionDTO):Transaction{
+    if(!this.all()[0]){
+      if(type=="outcome"){
+        throw Error("Saldo insuficiente")}
+
+      const transaction = new Transaction({title,value,type});
+      this.transactions.push(transaction);
+      return transaction;
+    }
+
+    const balance = this.getBalance();
+      if(type=="outcome" && (balance.total-value<0)){
+        throw Error("Saldo insuficiente")
+      }
+      else{
+    const transaction = new Transaction({title,value,type});
+        this.transactions.push(transaction);
+        return transaction;}
+
+
+
+
+}}
 
 export default TransactionsRepository;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request{
+  title:string;
+  value:number;
+  type:'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,9 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title,value,type}:Request): Transaction {
+
+
+    if(type!=="income"&&type!=="outcome"){
+      throw Error("Type of transaction unable")
+    }
+
+    const transaction =  this.transactionsRepository.create({
+      title,
+      value,
+      type})
+
+    return transaction
+
   }
+
 }
 
 export default CreateTransactionService;
