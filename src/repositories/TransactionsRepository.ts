bf0b47e76cdbb336af95c3a59dc82a167ec7a709
Luan Ramos <luanramossfarias@gmail.com>
2020-11-04 23:03:41 -0300

    commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -25,11 +25,11 @@ class TransactionsRepository {
 
   public getBalance(){
    const transactions = this.transactions;
-   const reducer = (accumulator:any, currentValue: any) =>{
-    return accumulator + currentValue;}
+   const reducer = (total:number, value: number) =>{
+    return total + value;}
 
-    function arrayConverter(type:"outcome"|"income",transaction:Transaction[]){
-      const arr = transaction.map(transaction=>{
+    function arrayConverter(type:"outcome"|"income"){
+      const arr = transactions.map(transaction=>{
         if(transaction.type==type){
           return transaction.value;
           }else{
@@ -37,8 +37,10 @@ class TransactionsRepository {
             }})
         return arr;}
 
-      const arrayIncome = arrayConverter("income",transactions)
-      const arrayOutcome = arrayConverter("outcome",transactions)
+
+
+      const arrayIncome = arrayConverter("income")
+      const arrayOutcome = arrayConverter("outcome")
       const income = arrayIncome.reduce(reducer);
       const outcome = arrayOutcome.reduce(reducer);
       const total = income-outcome;
@@ -52,7 +54,7 @@ class TransactionsRepository {
   public create({title,value,type}:CreateTransactionDTO):Transaction{
     if(!this.all()[0]){
       if(type=="outcome"){
-        throw Error("Saldo insuficiente")}
+        throw Error("insuficent balance")}
 
       const transaction = new Transaction({title,value,type});
       this.transactions.push(transaction);
@@ -61,7 +63,7 @@ class TransactionsRepository {
 
     const balance = this.getBalance();
       if(type=="outcome" && (balance.total-value<0)){
-        throw Error("Saldo insuficiente")
+        throw Error("insufficient funds");
       }
       else{
     const transaction = new Transaction({title,value,type});
